d5cc700f1c583ee7423c95041df207fcaf8d71c6
Madhu-92 <madhu.ktej@msitprogram.net>
2019-03-16 22:24:03 +0530

    Module-3

--- Module-3/Activity-8/S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ Module-3/Activity-8/S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,4 +1,15 @@
 package com.example.android.sunshine;
 
-public class DetailActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
 }
